2212e7058e80ff3b77ab2e749f9eabe76b0c3696
dsietz <davidsietz@yahoo.com>
2020-11-08 16:18:18 -0500

    working on order clothing service

--- rust-daas/src/bin/order_clothing.rs
+++ rust-daas/src/bin/order_clothing.rs
@@ -7,6 +7,42 @@ use kafka::consumer::{Consumer, FetchOffset, GroupOffsetStorage};
 use daas::service::processor::{DaaSProcessor, DaaSProcessorMessage, DaaSProcessorService};
 use std::sync::mpsc::{channel};
 use serde_json::value::Value;
+use std::fs;
+use std::fs::File;
+use std::io::prelude::*;
+use serde_json::json;
+
+static WORKSPACE_LOCAL_STORAGE: &str = "./workshop_storage";
+
+fn create_local_storage() {
+    match fs::create_dir_all(WORKSPACE_LOCAL_STORAGE) {
+        Ok(_) => {},
+        Err(err) => {
+            panic!("Warning: Could not create the local directory path {} : {:?}", WORKSPACE_LOCAL_STORAGE, err);
+        },
+    }
+}
+
+fn read_file(product_name: String) -> Option<Value> {
+    let path = format!("{}/clothing-{}.json", WORKSPACE_LOCAL_STORAGE, product_name);
+    let mut file = match File::open(path) {
+        Ok(f) => {
+            println!("Retreiving {} file", product_name);
+            f
+        },
+        Err(_e) => return None,
+    };
+    
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).unwrap();
+    
+    Some(serde_json::from_str(&contents).unwrap())
+}
+
+fn save_file(product_name: String, content: String) -> std::io::Result<()>{
+    let mut file = File::create(format!("{}/clothing-{}.json", WORKSPACE_LOCAL_STORAGE, product_name))?;
+    file.write_all(content.as_bytes())
+}
 
 
 fn main() {
@@ -26,6 +62,9 @@ fn main() {
                             .with_offset_storage(GroupOffsetStorage::Kafka)
                             .create()
                             .unwrap();
+                            
+    // Create the local storage directory for the aggregated data
+    create_local_storage();
 
     // start the processor
     let _handler = thread::spawn(move || {
@@ -34,7 +73,27 @@ fn main() {
             let order: Value = serde_json::from_str(&String::from_utf8(doc.data_obj_as_ref().to_vec()).unwrap()).unwrap();
 
             println!("Order Number {} from the {} has a status of {}...", doc.source_uid, doc.source_name, order.get("status").unwrap());
-            Ok(1)
+            
+            let prd = order.get("product").unwrap().as_str().unwrap().replace(" ","_").replace("/","");
+            let qnty = order.get("quantity").unwrap().as_u64().unwrap();
+            let content = match read_file(prd.clone().to_string()){
+                Some(mut obj) => {
+                    obj["orders"] = json!(obj["orders"].as_u64().unwrap() + qnty);
+                    obj
+                },
+                None => {
+                    let c = &format!("{{\"orders\":{:?}}}", qnty); 
+                    println!("{}",c);
+                    serde_json::from_str(c).unwrap()
+                },
+            };
+        
+            match save_file(prd.clone().to_string(), content.to_string()) {
+                Ok(_ok) => Ok(1),
+                Err(err) => {
+                    panic!("Warning: Could not save the clothing-{}.json file! : {:?}", prd, err);
+                },
+            }
         });
     });
 
